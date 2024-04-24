ba6533f822733c5d9aee50097b111ac60fc34bea
songshuai <275133280@qq.com>
2024-04-24 17:36:05 +0800

    chore: add type build

--- src/components/controls/button/button.vue
+++ src/components/controls/button/button.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
 import { onUnmounted, watch } from "vue";
-import { ContainersDefaultProps } from "../controls_props";
-import type { RectangleProps } from "../controls_props";
+import { ButtonDefaultProps } from "../controls_props";
+import type { ButtonProps } from "../controls_props";
 
 import useButton from "./button";
 import { compareAndset } from "../../../util";
-export interface ButtonProps extends  /* @vue-ignore */ RectangleProps {
-  thickness?: number;
-  cornerRadius?: number;
-  pointerEnterAnimation?: () => void;
-  pointerOutAnimation?: () => void;
-  pointerDownAnimation?: () => void;
-  pointerUpAnimation?: () => void;
-  delegatePickingToChildren?: boolean;
-}
-const props = withDefaults(defineProps<ButtonProps>(), ContainersDefaultProps);
+// export interface ButtonProps extends  /* @vue-ignore */ RectangleProps {
+//   thickness?: number;
+//   cornerRadius?: number;
+//   pointerEnterAnimation?: () => void;
+//   pointerOutAnimation?: () => void;
+//   pointerDownAnimation?: () => void;
+//   pointerUpAnimation?: () => void;
+//   delegatePickingToChildren?: boolean;
+// }
+const props = withDefaults(defineProps<ButtonProps>(), ButtonDefaultProps);
 const { node } = useButton<ButtonProps>(props);
 watch(
   props,
